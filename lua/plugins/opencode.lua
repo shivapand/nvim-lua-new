40369956bfbf157b3579dc039ec63dac92d6be3f
shivapand <Shiva.pandit@ecolibrium.io>
2026-02-13 09:32:03 +0530

    full-screen mode for opencide

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -8,18 +8,54 @@ return {
 			terminal = {}
 		}
 	} },
-	config = function()
-		vim.g.opencode_opts = {}
-
-		vim.o.autoread = true
-
-		vim.keymap.set(
-			{ 'n', 'x' },
+	keys = {
+		{
+			'<Leader>oc',
+			function() require('opencode').toggle() end,
+			desc = 'Toggle OpenCode',
+		},
+		{
 			'<C-x>',
+			function() require('opencode').select() end,
+			mode = { 'n', 'x' },
+			desc = 'Execute opencode action…',
+		},
+		{
+			'<Leader>of',
 			function()
-				require('opencode').select()
+				local provider = require('opencode.config').provider
+				if not provider or not provider.get then return end
+				local win = provider:get()
+				if not win or not win:valid() then return end
+
+				if win.opts.width == 0 then
+					win.opts.width = 120
+					win.opts.height = 30
+				else
+					win.opts.width = 0
+					win.opts.height = 0
+				end
+				win:update()
 			end,
-			{ desc = 'Execute opencode action…' }
-		)
-	end
+			desc = 'Toggle OpenCode fullscreen',
+		},
+	},
+	config = function()
+		vim.g.opencode_opts = {
+			provider = {
+				snacks = {
+					auto_close = true,
+					win = {
+						position = 'float',
+						width = 120,
+						height = 30,
+						border = 'rounded',
+						wo = { winbar = '' },
+						bo = { filetype = 'opencode_terminal' },
+					},
+				},
+			},
+		}
+		vim.o.autoread = true
+	end,
 }
