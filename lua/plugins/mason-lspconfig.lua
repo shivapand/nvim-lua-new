573e07adac391d641819f39b50e07f2a31e9d841
shivapand <Shiva.pandit@ecolibrium.io>
2026-02-13 08:45:44 +0530

    mason deprication issues addressed by calude code

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -13,9 +13,6 @@ return {
 		version = '*'
 	}, { 'b0o/schemastore.nvim' } },
 	config = function()
-		local lspconfig = require('lspconfig')
-		local util = require('lspconfig.util')
-
 		require('mason-tool-installer').setup({
 			ensure_installed = {
 				'lua-language-server',
@@ -30,36 +27,19 @@ return {
 			}
 		})
 
-		require('mason-lspconfig').setup({
-			automatic_installation = true,
-			automatic_enable = false
+		vim.lsp.config('jsonls', {
+			settings = {
+				json = {
+					schemas = require('schemastore').json.schemas(),
+					validate = { enable = true }
+				}
+			}
 		})
 
-		local default_handler = function(server_name)
-			lspconfig[server_name].setup({})
-		end
-
-		local custom_handlers = { jsonls = function()
-			lspconfig.jsonls.setup({
-				settings = {
-					json = {
-						schemas = require('schemastore').json.schemas(),
-						validate = { enable = true }
-					}
-				}
-			})
-		end }
-
-		local installed_servers =
-			require('mason-lspconfig').get_installed_servers()
-
-		for _, server_name in ipairs(installed_servers) do
-			if custom_handlers[server_name] then
-				custom_handlers[server_name]()
-			else
-				default_handler(server_name)
-			end
-		end
+		require('mason-lspconfig').setup({
+			automatic_installation = true,
+			automatic_enable = true
+		})
 
 		vim.diagnostic.config({
 			severity_sort = true,
